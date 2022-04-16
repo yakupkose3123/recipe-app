6e8415be9f48c23d3bd600ec4003b0fb8ed69f18
yakupkose3123 <yakupkose3123@gmail.com>
2022-04-16 17:00:49 +0300

    created details page

--- src/pages/recipe/RecipeCard.js
+++ src/pages/recipe/RecipeCard.js
@@ -1,13 +1,19 @@
+import {  useNavigate } from "react-router-dom"
 import Styles from "./Recipe.module.css"
 
 const RecipeCard = ({data}) => {
   // console.log(data.recipe);
+  const navigate = useNavigate();
+
+  const moreClick = () => {    
+    navigate("/details", {state:{data}});
+  }
 
   return (
     <div className={Styles.recipeCards}>
         <h3>{data.recipe.label}</h3>
         <img src={data.recipe.image} alt="recipeImage"/>
-        <button className={Styles.showButton}>Show More</button>        
+        <button className={Styles.showButton} onClick={moreClick}>Show More</button>        
     </div>
   )
 }
